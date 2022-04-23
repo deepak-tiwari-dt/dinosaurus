c323dc545acdd17ddaae4e02692cf5e5d4ed0558
Deepak Tiwari <deepak.tiwari.d21@gmail.com>
2022-04-23 21:22:42 +0530

    Dinosaurs json update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import express from "express";
+import dinosaurs from "./dinosaurs.json" assert { type: "json" };
+const app = express();
 
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.json(dinosaurs);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
